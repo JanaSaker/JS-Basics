bb812f958fd8600731dd6d6ef8ee64f1cd503a66
JanaSaker <janasakr2000@gmail.com>
2023-08-30 15:50:16 +0300

    completed step2

---
+++ javascript-basics-01/step_2.js
@@ -0,0 +1,8 @@
+// Define variables
+let name = "Jana";
+let surname = "Sakr";
+let city = "Beirut";
+
+// Display the variables
+alert("Name: "+ name + "\n"+"Surname:" + surname+
+"\n"+"City: " + city)

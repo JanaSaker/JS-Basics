11eea3464195a5cf413dfe5e89f2a26b882a2e30
JanaSaker <janasakr2000@gmail.com>
2023-08-30 16:28:18 +0300

    completed for step5

---
+++ javascript-basics-01/step_6.js
@@ -0,0 +1,16 @@
+var firstNumberInput = document.getElementById("first_number");
+var secondNumberInput = document.getElementById("second_number");
+var validateButton = document.getElementById("validate");
+
+validateButton.addEventListener("click", function() {
+    var firstNumber = parseFloat(firstNumberInput.value);
+    var secondNumber = parseFloat(secondNumberInput.value);
+
+    if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+        var result = firstNumber * secondNumber;
+
+        alert("The result of multiplying " + firstNumber + " and " + secondNumber + " is: " + result);
+    } else {
+        alert("Please enter valid numbers.");
+    }
+});

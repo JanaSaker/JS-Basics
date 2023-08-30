80ecf8a4278aaced4842b6bff100e1ded088672d
JanaSaker <janasakr2000@gmail.com>
2023-08-30 15:58:57 +0300

    completed for step4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,4 @@
+let name = prompt("Please enter your name:");
+let surname= prompt("please enter your surname:");
+let city =prompt("please enter yourr city:")
+alert("Hello, " + name + surname +"\n"+city+"\n");
